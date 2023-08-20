5b60d2a7ce56dce28dc95b10c41dfc60276a8d65
Massimiliano Viola <68594973+massimilianoviola@users.noreply.github.com>
2023-08-20 09:25:28 +0200

    slurm script for sequential

---
+++ write_slurm_seq.py
@@ -0,0 +1,22 @@
+sizes = ['256', '384', '512', '768']
+
+for size in sizes:
+    with open(f'slurm_script/{size}_seq.slurm', 'w') as f:
+        f.write(f'#!/bin/bash\n')
+        f.write(f'\n')
+        f.write(f'#SBATCH --job-name mmult\n')
+        f.write(f'#SBATCH --output {size}/plain_seq_%j.out\n')
+        f.write(f'#SBATCH --error {size}/plain_seq_%j.err\n')
+        f.write(f'#SBATCH --mail-user massimiliano.viola@studenti.unipd.it\n')
+        f.write(f'#SBATCH --mail-type ALL\n')
+        f.write(f'#SBATCH --time 10:00\n')
+        f.write(f'#SBATCH --ntasks 1\n')
+        f.write(f'#SBATCH --partition allgroups\n')
+        f.write(f'#SBATCH --mem 200M\n')
+        f.write(f'\n')
+        f.write(f'\n')
+        f.write(f'gcc -o sequential -O3 sequential.c\n')
+        f.write(f'\n')
+        f.write(f'for ((i = 0; i < 10; i++)) do\n')
+        f.write(f'    ./sequential {size}\n')
+        f.write(f'done\n')
